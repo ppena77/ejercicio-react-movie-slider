6bd7f8e96b9f216c60a04a759386ee7711ff79fe
ppena77 <ppena77@alumno.uned.es>
2022-07-09 15:13:46 +0200

    Changed navigation method from Link to useNavigate

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,19 @@
 import './MovieCard.css';
-import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 function MovieCard (props) {
+    const navigate = useNavigate();
+    function toNavigate() {
+        navigate('/pelicula');
+    };
 
     const imgSrcDomain = 'https://image.tmdb.org/t/p/w220_and_h330_face';
     
     return (
-        <div className='movie-cards__card'>
-            <Link to="/pelicula">
-                <button>
-                    <img src={imgSrcDomain + props.movieData.poster_path} alt={props.movieData.title} title={props.movieData.title} />
-                </button>
-            </Link>
+        <div className='movie-cards__card'>            
+            <button onClick={toNavigate}>
+                <img src={imgSrcDomain + props.movieData.poster_path} alt={props.movieData.title} title={props.movieData.title} />
+            </button>            
         </div>
     )    
 };
