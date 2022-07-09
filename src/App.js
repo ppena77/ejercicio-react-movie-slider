1efc8fa0bb17f4f3fd34caf71228f5f66869d781
ppena77 <ppena77@alumno.uned.es>
2022-07-09 13:39:15 +0200

    Added Home component to wrap MovieCardsContainer in it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+//import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
 import MovieCardsContainer from './components/MovieCardsContainer';
 
 function App() {
+  {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter> */}
+
   return (
-    <div className="App">
-      <h1>Listado Peliculas</h1>
-      <MovieCardsContainer />
-    </div>
+      <div>
+        <Home />
+      </div>            
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+import MovieCardsContainer from "./MovieCardsContainer";
+
+function Home () {
+    return (
+        <>
+            <h1>Películas Recientes</h1>
+            <MovieCardsContainer />
+        </>
+    )
+}
+
+export default Home;
